6bbf813d10a990ece76d9c5578c1a49633cfd301
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-05-15 15:03:52 +0530

    images added

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -1,75 +1,96 @@
+import 'dart:convert';
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(2, 1),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(3, 1),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(4, 1),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(3, 2)
-];
-List<Widget> _tiles = const <Widget>[
-  const Tile(Colors.green, Icons.widgets),
-  const Tile(Colors.lightBlue, Icons.wifi),
-  const Tile(Colors.amber, Icons.panorama_wide_angle),
-  const Tile(Colors.brown, Icons.map),
-  const Tile(Colors.deepOrange, Icons.send),
-  const Tile(Colors.indigo, Icons.airline_seat_flat),
-  const Tile(Colors.red, Icons.bluetooth),
-  const Tile(Colors.pink, Icons.battery_alert),
-  const Tile(Colors.purple, Icons.desktop_windows),
-  const Tile(Colors.blue, Icons.radio),
-  const Tile(Colors.lightGreen,Icons.ac_unit),
-  const Tile(Colors.lightBlueAccent,Icons.account_balance)
-];
-class Images extends StatelessWidget {
+import 'package:http/http.dart' as http;
+ Map<String,dynamic> _data;
+  List<dynamic> _results;
+
+class Images extends StatefulWidget {
+ 
 
 
   @override
+  _ImagesState createState() => _ImagesState();
+}
+
+class _ImagesState extends State<Images> {
+ 
+    Future<String> _getResponse() async //url get request
+  {
+    http.Response _response = await http.get("https://backend.scrapshut.com/api/img/");
+    //print(_response.body);
+    _data =  jsonDecode(_response.body);
+    print(_data);
+    setState(() {
+      _results = _data['results'];
+    });
+    
+    print(_results);
+    print(_results[0]["picture"]);
+    print(_results.length);
+    
+    //setting is fetching false to display data
+    return "Success";
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _getResponse();
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container(
-         child :Padding(
-            padding: const EdgeInsets.only(top: 12.0),
-            child: new StaggeredGridView.count(
-              crossAxisCount: 4,
-              staggeredTiles: _staggeredTiles,
-              children: _tiles,
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
-              padding: const EdgeInsets.all(4.0),
-            )));
+   return  StaggeredGridView.countBuilder(
+  crossAxisCount: 4,
+  itemCount: _results.length,
+  itemBuilder: (BuildContext context, int index) => Tile(_results[index]["picture"]),
+ 
+  staggeredTileBuilder: (int index) =>
+      new StaggeredTile.count(2,  index.isEven ? 4 : 3),
+ 
+  
+);
+   
       
     
   }
 }
 class Tile extends StatelessWidget {
-  const Tile(this.backgroundColor, this.iconData);
-  final Color backgroundColor;
-  final IconData iconData;
+  const Tile( this.url);
+
+  final String url;
   @override
   Widget build(BuildContext context) {
-    return new Card(
-      color: backgroundColor,
-      child: new InkWell(
+    return Card(
+      
+      child:  InkWell(
         onTap: () {},
-        child: new Center(
-          child: new Padding(
-            padding: const EdgeInsets.all(4.0),
+       
+          
+           child:  new Center(
+              child: new Padding(
+                padding: const EdgeInsets.all(4.0),
+                
+                 child: new Container(
+                   
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(10),
+                     image: DecorationImage(
+                       image: NetworkImage(url,),
+                       fit: BoxFit.cover
+                     )
+                   ),
+                 )
             
-                         
-                                                      child: new Icon(
-                iconData,
-                color: Colors.white,
-              ),
-                          ),
-            ),
           ),
+                  
+                              ),
+                
+          ),
+      
         
       
     );

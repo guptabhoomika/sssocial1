4e001f37857ad9e119d4db66c09f5e76ff6ed621
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-05-18 18:29:07 +0530

    delete

--- lib/pages/userprofile.dart
+++ lib/pages/userprofile.dart
@@ -96,6 +96,31 @@ List<dynamic> _results; //stores the list of result of url
    });
     print(_msgresults.length);
   }
+  Future<http.Response> delete(int id) async {
+     String bvalue = await storage.read(key: 'btoken');
+    print("in delete");
+    print(id);
+    Map<String, String> headers = {"Authorization":"JWT $bvalue",
+          "Content-Type":"application/json"};
+          print(headers);
+          final http.Response response = await http.delete(
+    'https://backend.scrapshut.com/user/post/$id',
+    headers:  headers
+    
+  );
+  
+   Navigator.of(context, rootNavigator: true).pop();
+  print("delete");
+  //var r =  jsonDecode(response.body);
+  //print(r);
+  print(response.statusCode);
+ 
+  // print("making get req again");
+  _getTabResponse();
+  
+  
+  return response;
+}
  
 
   TabController _tabController;
@@ -194,14 +219,14 @@ List<dynamic> _results; //stores the list of result of url
                    ListView.builder(
           itemCount: _results.length,
           itemBuilder: (context,index){
-            return display(_results[index]['rate'], _results[index]['author'], _results[index]['url'], _results[index]["created_at"],  _results[index]['content'], _results[index]['tags'],false);
+            return display(_results[index]['rate'], _results[index]['author'], _results[index]['url'], _results[index]["created_at"],  _results[index]['content'], _results[index]['tags'],false,_results[index]["id"]);
           },
          ),
         //Text("url"),
                    ListView.builder(
         itemCount: _msgresults.length,
         itemBuilder: (context,index){
-          return display(_msgresults[index]['rate'], _msgresults[index]['author'], _msgresults[index]['review'], _msgresults[index]["created_at"],  _msgresults[index]['content'], _msgresults[index]['tags'],true);
+          return display(_msgresults[index]['rate'], _msgresults[index]['author'], _msgresults[index]['review'], _msgresults[index]["created_at"],  _msgresults[index]['content'], _msgresults[index]['tags'],true,_msgresults[index]["id"]);
         },
       ),
                   //Text("ms"),
@@ -220,92 +245,141 @@ List<dynamic> _results; //stores the list of result of url
     );
   }
 
-  Container display(int rate,String author,String url,String time,String content,List tags,bool isMsg)
+  InkWell display(int rate,String author,String url,String time,String content,List tags,bool isMsg,int id)
   
   {
   String c = time.substring(0,10);
-    return Container(
-      margin: EdgeInsets.all(8),
-      height: 120,
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 40,
-            color: Colors.grey[300],
-            
-            
-            child: Column(
-              children: <Widget>[
-                Text(""),
-                Icon(Icons.arrow_upward,),
-                SizedBox(height: 5,),
-                Text(rate.toString(),style: TextStyle(fontWeight: FontWeight.bold),),
-                SizedBox(height: 5,),
-                Icon(Icons.arrow_downward)
-              ],
-            ),
-            
-          ),
-          SizedBox(width: 10,),
-          Container(
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: <Widget>[
-               Row(
-                 children: <Widget>[
-                   Icon(Icons.blur_circular,color: Colors.blue,),
-                   Text("r/news ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 10),),
-                   Text("Posted by u/",style: TextStyle(color: Colors.grey,fontSize: 10),),
-                   Text(author + " ",style: TextStyle(color: Colors.grey,fontSize: 10)),
-                   Text( "on " + c ,style: TextStyle(color: Colors.grey,fontSize: 7)),
-                   
-                   
+    return InkWell(
+      onLongPress: (){
+            print("Preess");
+        showAlertDialog(context,id,isMsg);
+      
+      },
+          child: Container(
+        margin: EdgeInsets.all(8),
+        height: 120,
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 40,
+              color: Colors.grey[300],
               
-                  
-                   //Spacer(),
-                 ],
-               ),
-               SizedBox(height: 10,),
-               Text(content,style: TextStyle(fontWeight: FontWeight.bold),),
+              
+              child: Column(
+                children: <Widget>[
+                  Text(""),
+                  Icon(Icons.arrow_upward,),
                   SizedBox(height: 5,),
-                 tags.isNotEmpty ?  Text( "Tags: "+ tags.toString().replaceAll("[", " ").replaceAll("]", " " ),style: TextStyle(color: Colors.grey,fontSize: 10 )): Text("No tags ",style: TextStyle(color: Colors.grey,fontSize: 10 )),
-               SizedBox(height: 5,),
+                  Text(rate.toString(),style: TextStyle(fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
+                  Icon(Icons.arrow_downward)
+                ],
+              ),
               
-               
-               Row(
-                 children: <Widget>[
-                  Text( isMsg==true ? url : url.substring(0,10),style: TextStyle(color: Colors.blue,fontSize: isMsg? 15 : 10 ),),
-                 isMsg==true? Text(" ") : Icon(Icons.call_made,color: Colors.blue,size: 10,)
+            ),
+            SizedBox(width: 10,),
+            Container(
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: <Widget>[
+                 Row(
+                   children: <Widget>[
+                     Icon(Icons.blur_circular,color: Colors.blue,),
+                     Text("r/news ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 10),),
+                     Text("Posted by u/",style: TextStyle(color: Colors.grey,fontSize: 10),),
+                     Text(author + " ",style: TextStyle(color: Colors.grey,fontSize: 10)),
+                     Text( "on " + c ,style: TextStyle(color: Colors.grey,fontSize: 7)),
+                     
+                     
+                
+                    
+                     //Spacer(),
+                   ],
+                 ),
+                 SizedBox(height: 10,),
+                 Text(content,style: TextStyle(fontWeight: FontWeight.bold),),
+                    SizedBox(height: 5,),
+                   tags.isNotEmpty ?  Text( "Tags: "+ tags.toString().replaceAll("[", " ").replaceAll("]", " " ),style: TextStyle(color: Colors.grey,fontSize: 10 )): Text("No tags ",style: TextStyle(color: Colors.grey,fontSize: 10 )),
+                 SizedBox(height: 5,),
+                
+                 
+                 Row(
+                   children: <Widget>[
+                    Text( isMsg==true ? url : url.substring(0,10),style: TextStyle(color: Colors.blue,fontSize: isMsg? 15 : 10 ),),
+                   isMsg==true? Text(" ") : Icon(Icons.call_made,color: Colors.blue,size: 10,)
 
-               ],),
-              
-               Spacer(),
-               Row(
-                 children: <Widget>[
-                   Icon(Icons.message,color: Colors.grey,),
-                   SizedBox(width: 5,),
-                   Text("Comments",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
-                   SizedBox(width: 5,),
-                   Icon(Icons.share,color: Colors.grey,),
-                   SizedBox(width: 5,),
-                   Text("Share",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                   SizedBox(width: 5,),
-                   Icon(Icons.save_alt,color: Colors.grey),
-                   SizedBox(width: 5,),
-                   Text("Save",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
-                   SizedBox(width: 5,),
-                   Text("...",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold))
-                 ],
-               )
+                 ],),
+                
+                 Spacer(),
+                 Row(
+                   children: <Widget>[
+                     Icon(Icons.message,color: Colors.grey,),
+                     SizedBox(width: 5,),
+                     Text("Comments",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                     SizedBox(width: 5,),
+                     Icon(Icons.share,color: Colors.grey,),
+                     SizedBox(width: 5,),
+                     Text("Share",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
+                     SizedBox(width: 5,),
+                     Icon(Icons.save_alt,color: Colors.grey),
+                     SizedBox(width: 5,),
+                     Text("Save",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold)),
+                     SizedBox(width: 5,),
+                     Text("...",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold))
+                   ],
+                 )
 
-             ],
+               ],
 
+              )
             )
-          )
-        ],
+          ],
+        ),
       ),
     );
 
   }
+  showAlertDialog(BuildContext context,int id,bool msg) {  
+  // Create button  
+  
+  Widget continueButton = FlatButton(
+    child: Text("Continue"),
+    onPressed:  () {
+      if(msg==false)
+      {
+           delete(id);
+      }
+      else
+      {
+        print("Tap");
+      }
+   
+    },
+  );
+   Widget cancel = FlatButton(
+    child: Text("Cancel"),
+    onPressed:  () {
+      Navigator.of(context, rootNavigator: true).pop();
+    },
+  );
+  
+  // Create AlertDialog  
+  AlertDialog alert = AlertDialog(  
+    title: Text("Delete item?"),  
+       actions: [  
+         cancel,
+     continueButton  
+
+    ],  
+  );  
+  
+  // show the dialog  
+  showDialog(  
+    context: context,  
+    builder: (BuildContext context) {  
+      return alert;  
+    },  
+  );  
+}  
 }
  
